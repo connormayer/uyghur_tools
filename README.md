60541c601618716266ca664bbc0939cab7e10c6b
Connor Mayer <connor.joseph.mayer@gmail.com>
2018-05-21 19:40:06 -0700

    Added wugger

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 Miscellaneous scripts for processing Uyghur data
 
 * uyghur_latin_to_cyrillic.py: Converts text files containing latin Uyghur orthography to cyrillic orthography.
+
+    usage: python3 uyghur_latin_to_cyrillic.py <input_file> <output_file>
+
+* uyghur_wugger.py: Generates Uyghur wug words following a template file.
+
+    usage: python3 uyghur_wugger.py <template file> <output file>
+
+    ====================
+    Template file format
+    ====================
+
+    Each line is a tab-separated pair: 
+        <form template> <number>
+
+    The form template field specifies the general segmental properties of the words. There are currently 6 segment types:
+
+        C: Non-harmonizing consonants
+        V: Non-harmonizing vowels
+        F: Front vowels
+        B: Back vowels
+        K: Front dorsals
+        Q: Back dorsals
+
+    These can be used in any combination. For example, CFQ would generate three-segment words that start with a non-harmonizing consonant, followed by a front vowel, followed by a back dorsal.
+
+    The number field specifies how many tokens of each template are created.
+
+    The actual segments that go into each segment type slot are chosen uniformly at random from that class of segments. See the source code for details. See wug_templates.txt for an example of the template file.
+
+* uyghur_regexes.txt: Some example regexes for use in a Uyghur corpus searcher
---
+++ uyghur_wugger.py
@@ -0,0 +1,63 @@
+import argparse
+import random
+
+SOUND_DICT = {
+    'C': [
+        'b', 'p', 't', 'j', 'x', 'd', 'r', 'z', 's', 'f', 'l', 'm', 'n', 'h',
+        'w', 'y', 'ng', 'sh', 'zh', 'ch'
+    ],
+
+    'V': [
+        'i', 'é'   
+    ],
+
+    'F': [
+        'e', 'ö', 'ü'
+    ],
+
+    'B': [
+        'a', 'o', 'u'
+    ],
+
+    'K': [
+        'k', 'g', 
+    ],
+
+    'Q': [
+        'q', 'gh'
+    ]
+}
+
+def create_wugs(template, number):
+    wugs = []
+    for _ in range(int(number)):
+        wug = ''
+        for c in template:
+            wug += random.choice(SOUND_DICT[c])
+        wugs.append(wug)
+    return wugs
+
+def generate_wugs(input_file, output_file):
+    wugs = []
+    with open(input_file, 'r') as f:
+        for line in f:
+            if line and not line[0] == '#':
+                template, number = line.rstrip().split('\t')
+                wugs.extend(create_wugs(template, number))
+
+    with open(output_file, 'w') as f:
+        for wug in wugs:
+            f.write(wug + '\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate Uyghur wug words from a template file"
+    )
+    parser.add_argument(
+        "infile", type=str, help="The file to read the templates from"
+    )
+    parser.add_argument(
+        "outfile", type=str, help="The file to write wugs to"
+    )
+    args = parser.parse_args()
+    generate_wugs(args.infile, args.outfile)
